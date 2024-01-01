9a59ddd10bdb3142cd5372053152d0eef73f4522
zemmouriahmed <mourizem@gmail.com>
2024-01-01 12:44:53 +0100

    push pour calculatrice en python

---
+++ calculatrice.py
@@ -0,0 +1,46 @@
+def addition(a,b):
+    return a+b
+
+def soustraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    if b != 0 : 
+     return a/b
+    else:
+        print("la division par 0 n'existe pas")
+
+print()
+    
+N1 = float(input("entrer premier nombre : "))
+
+print()
+
+N2 = float(input("entrer second nombre :"))
+
+print()
+
+Op = input("entrer opération :")
+
+if Op == '+':
+            resultat = addition(N1,N2)
+
+elif Op == '*':
+            resultat = multiplication(N1,N2)
+
+elif Op == '-' :
+            resultat = soustraction(N1,N2)
+
+elif Op == '/' :
+            resultat = division(N1,N2)
+
+else :
+            print("vérifier vos entrées")
+
+print()                    
+
+print(f"{N1} {Op} {N2} = {resultat}")
+

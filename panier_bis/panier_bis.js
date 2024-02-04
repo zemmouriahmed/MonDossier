fd67511a9ed1a9df51acbe0fb804c4dc2e079d27
zemmouriahmed <mourizem@gmail.com>
2024-02-04 07:50:52 +0100

    nouvelle config

---
+++ panier_bis/panier_bis.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Tableau d'objets avec les valeurs pour card-text et card-title
+  const cardData = [
+    {
+      title: "BANANE",
+      text: "15 DH/KG",
+    },
+    {
+      title: "CAROTTE",
+      text: "8 DH/KG",
+    },
+    {
+      title: "POIRE",
+      text: "45 DH/KG",
+    },
+    {
+      title: "POMME DE TERRE",
+      text: "11 DH/KG",
+    },
+    {
+      title: "POMME",
+      text: "32 DH/KG",
+    },
+    {
+      title: "TOMATE",
+      text: "5 DH/KG",
+    },
+  ];
+
+  // Créer un nouveau tableau d'objets où la partie 'text' ne comprendra que la partie numérique
+  const newData = cardData.map((item) => {
+    const numericPart = item.text.match(/\d+/); // Recherche des nombres dans la chaîne 'text'
+    const numericValue = numericPart ? parseInt(numericPart[0]) : null; // Convertir en nombre entier
+    return { title: item.title, text: numericValue }; // Retourne un nouvel objet avec le titre et la partie numérique de 'text'
+  });
+
+  // Sélectionner tous les éléments avec les classes card-text et card-title
+  const cardTextElements = document.querySelectorAll(".card-text");
+  const cardTitleElements = document.querySelectorAll(".card-title");
+
+  // Mettre à jour le texte dans les éléments card-text et card-title
+  cardTextElements.forEach((element, index) => {
+    element.textContent = newData[index].text;
+  });
+
+  cardTitleElements.forEach((element, index) => {
+    element.textContent = newData[index].title;
+  });
+
+
+
+
+    const cart = {}; // Objet pour stocker les articles dans le panier
+
+    // Sélectionner l'élément "rectangle" où la liste des objets sélectionnés sera affichée
+    const rectangle = document.querySelector('.rectangle');
+
+    // Fonction pour mettre à jour l'affichage des objets sélectionnés dans le rectangle
+    function updateCartDisplay() {
+        // Générer la liste des objets sélectionnés
+        const cartItems = Object.entries(cart).map(([title, quantity]) => {
+            return `${quantity} ${title}`;
+        });
+
+        // Mettre à jour le contenu de l'élément "rectangle" avec la liste en colonne
+        rectangle.innerHTML = cartItems.map(item => `<div>${item}</div>`).join('');
+    }
+
+    const plusButtons = document.querySelectorAll('.btn-plus, .btn-plusbis');
+    const minusButtons = document.querySelectorAll('.btn-minus');
+
+    plusButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const title = cardData[index].title;
+            if (cart[title]) {
+                cart[title]++;
+            } else {
+                cart[title] = 1;
+            }
+            updateCartDisplay(); // Mettre à jour l'affichage des objets sélectionnés
+            console.log(cart);
+        });
+    });
+
+    minusButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const title = cardData[index].title;
+            if (cart[title] && cart[title] > 0) {
+                cart[title]--;
+                if (cart[title] === 0) {
+                    delete cart[title]; // Supprimer l'article du panier si la quantité atteint zéro
+                }
+                updateCartDisplay(); // Mettre à jour l'affichage des objets sélectionnés
+                console.log(cart);
+            }
+        });
+    });
+
+    // Gestionnaire d'événement pour le bouton de panier
+    const cartButton = document.querySelector('.btn-panier');
+    cartButton.addEventListener('click', () => {
+        // Générer la liste de la commande et le total des prix
+        const orderList = Object.entries(cart).map(([title, quantity]) => {
+            const item = cardData.find(item => item.title === title);
+            const total = quantity * parseInt(item.text.match(/\d+/)[0]);
+            return `${quantity} ${title}: ${total} DH`;
+        });
+        const totalPrice = Object.entries(cart).reduce((acc, [title, quantity]) => {
+            const item = cardData.find(item => item.title === title);
+            const price = parseInt(item.text.match(/\d+/)[0]);
+            return acc + (quantity * price);
+        }, 0);
+        
+        // Afficher une fenêtre avec la liste de la commande et le total des prix
+        alert(`CONTENU DE VOTRE PANIER :\n\n${orderList.join('\n')}\n\nTotal : ${totalPrice} DH`);
+    });
+
+
+
+
+});
--- trilisttupl.py
+++
@@ -1,7 +0,0 @@
-def trier_par_dernier_element(liste_de_tuples):
-
-    liste_de_tuples.sort(key=lambda x:x[-1])
-    liste_tuples_str = input("entrer une liste de tuples (séparer les éléments par des virgules et les tuples par des point virgules ):")
-    liste_tuples=[tuple(map(int,tuple_str.split(','))) for tuple_str in liste_de_tuples.split(';')]
-    trier_par_dernier_element(liste_tuples)
-    print(f"liste des tuples triée par ordre croissant des derniers éléments: {liste_tuples}")
